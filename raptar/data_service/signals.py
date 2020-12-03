05b94c2aa46be4f90527f84e8dae09a924f91f24
Ashwin Krishna <ashwin.krishna@betsol.com>
2020-12-02 20:06:44 -0700

    fix rule bug

--- raptar/data_service/signals.py
+++ raptar/data_service/signals.py
@@ -2,10 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from raptar.data_service.models.testreport import TestReport
-from raptar.data_service.rules.testreport_rules import TestReportBusinessRule
+from raptar.data_service.rules import TestReportBusinessRule
 import logging
 
 @receiver(post_save, sender=TestReport)
 def execute_product_business_rules(sender, instance, **kwargs):
-    logging.info("Signal Received to exeute Test Report Business Rules")
+    logging.info("Signal Received to execute Test Report Business Rules")
     TestReportBusinessRule.run_all(instance)
